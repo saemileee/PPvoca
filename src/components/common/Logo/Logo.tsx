4c5889a362e5a516cc71b18dfaffffbeda7aeb13
icallitnewart <icallitnewart@gmail.com>
2023-05-12 02:20:26 +0900

    Design: 로고 컴포넌트 레이아웃 구현
    로고 컴포넌트 레이아웃 구현 및 디자인 작업
    로고 이미지는 추후 수정 예정

---
+++ src/components/common/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Logo.module.scss';
+
+type LogoProps = {
+	style: React.CSSProperties;
+};
+
+function Logo({ style }: LogoProps) {
+	return (
+		<div className={styles.container} style={style}>
+			<div className={styles.image}>logo</div>
+			<h1>뿅뿅 단어장</h1>
+		</div>
+	);
+}
+
+export default Logo;
