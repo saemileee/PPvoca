b728ee89b265e531471f82cd76868392bb28d2e5
icallitnewart <icallitnewart@gmail.com>
2023-05-12 02:33:32 +0900

    Design: Register 페이지 레이아웃 구현
    Register(회원가입) 페이지 레이아웃 구현 및 디자인 작업

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from '../components/User/Register.module.scss';
+import Logo from '../components/common/Logo/Logo';
+import UserInput from '../components/User/UserInput/UserInput';
+import UserButton from '../components/User/UserButton/UserButton';
 
 function Register() {
+	const logoStyle = {
+		transform: 'translateX(-10px)',
+		marginBottom: '5vh',
+	};
+
+	const initValues = {
+		email: '',
+		nickname: '',
+		password: '',
+		passwordConfirm: '',
+	};
+
+	const [values, setValues] = useState(initValues);
+
 	return (
-		<main>
-			<h1>Register</h1>
+		<main className={styles.container}>
+			<Logo style={logoStyle} />
+			<form className={styles.form}>
+				<ul>
+					<li>
+						<UserInput
+							type='text'
+							name='email'
+							label='이메일'
+							setValues={setValues}
+						/>
+					</li>
+					<li>
+						<UserInput
+							type='text'
+							name='nickname'
+							label='닉네임'
+							setValues={setValues}
+						/>
+					</li>
+					<li>
+						<UserInput
+							type='password'
+							name='password'
+							label='비밀번호'
+							setValues={setValues}
+						/>
+					</li>
+					<li>
+						<UserInput
+							type='password'
+							name='passwordConfirm'
+							label='비밀번호 확인'
+							setValues={setValues}
+						/>
+					</li>
+					<li>
+						<UserButton type='submit' onClick={() => console.log('join')}>
+							가입하기
+						</UserButton>
+					</li>
+				</ul>
+			</form>
 		</main>
 	);
 }
