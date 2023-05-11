fa249ae451c3e7bec24f436a774e16366f763333
icallitnewart <icallitnewart@gmail.com>
2023-05-12 02:32:01 +0900

    Design: Login 페이지 레이아웃 구현
    Login 페이지 레이아웃 구현 및 디자인 작업

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styles from '../components/User/Login.module.scss';
+import Logo from '../components/common/Logo/Logo';
+import UserInput from '../components/User/UserInput/UserInput';
+import UserButton from '../components/User/UserButton/UserButton';
 
 function Login() {
+	const logoStyle = {
+		transform: 'translateX(-10px)',
+		marginBottom: '5vh',
+	};
+
+	const initValues = {
+		email: '',
+		password: '',
+	};
+
+	const [values, setValues] = useState(initValues);
+
+	useEffect(() => {
+		console.log(values);
+	}, [values]);
+
 	return (
-		<main>
-			<h1>Login</h1>
+		<main className={styles.container}>
+			<Logo style={logoStyle} />
+			<form className={styles.form}>
+				<ul>
+					<li>
+						<UserInput
+							type='text'
+							name='email'
+							label='이메일'
+							setValues={setValues}
+						/>
+					</li>
+					<li>
+						<UserInput
+							type='password'
+							name='password'
+							label='비밀번호'
+							setValues={setValues}
+						/>
+					</li>
+					<li>
+						<UserButton type='submit' onClick={() => console.log('login')}>
+							로그인
+						</UserButton>
+					</li>
+					<li>
+						<UserButton type='link' path='/register'>
+							회원가입
+						</UserButton>
+					</li>
+				</ul>
+			</form>
 		</main>
 	);
 }
