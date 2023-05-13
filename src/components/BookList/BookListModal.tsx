358a80af0561759b9ebd0ebe8cec57d134dd4494
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-05-13 15:50:55 +0900

    Refactor: 모달기능 컴포넌트화

---
+++ src/components/BookList/BookListModal.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect } from 'react';
+import styles from './Style.module.scss';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { HiOutlinePencil } from 'react-icons/hi';
+
+interface ModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	onEdit: () => void;
+	onDelete: () => void;
+}
+
+function Modal({ isOpen, onClose, onEdit, onDelete }: ModalProps) {
+	const modalRef = useRef<HTMLDivElement>(null);
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+			onClose();
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	});
+
+	if (!isOpen) {
+		return null;
+	}
+
+	return (
+		<div ref={modalRef} className={styles.modal}>
+			<button onClick={onEdit}>
+				<HiOutlinePencil size={24} />
+				수정
+			</button>
+			<button onClick={onDelete}>
+				<AiOutlineDelete size={24} />
+				삭제
+			</button>
+		</div>
+	);
+}
+
+export default Modal;
