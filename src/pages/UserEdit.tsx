5d165ba5eabc8b38e0da07c48ec33940a92a9382
icallitnewart <icallitnewart@gmail.com>
2023-05-13 10:17:48 +0900

    Feat: UserEdit 페이지 유효성 검사 적용
    UserEdit 페이지 유효성 검사 적용, 추후 수정 필요

--- src/pages/UserEdit.tsx
+++ src/pages/UserEdit.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import useUserValidator from '../hooks/useUserValidator';
 import styles from '../components/User/Register.module.scss';
 import Logo from '../components/common/Logo/Logo';
 import UserInput from '../components/User/UserInput/UserInput';
@@ -17,6 +18,12 @@ function UserEdit() {
 	};
 
 	const [values, setValues] = useState(initValues);
+	const { errors, userValidator, validationPass } =
+		useUserValidator(initValues);
+
+	useEffect(() => {
+		if (validationPass) alert('유효성 검사 통과!!!');
+	}, [validationPass]);
 
 	return (
 		<main className={styles.container}>
@@ -37,7 +44,10 @@ function UserEdit() {
 							type='text'
 							name='nickname'
 							label='닉네임'
+							defaultValue='엘리스'
 							setValues={setValues}
+							condition='10자 이하 (특수 문자, 공백 제외)'
+							error={errors.nickname}
 						/>
 					</li>
 					<li>
@@ -46,6 +56,8 @@ function UserEdit() {
 							name='password'
 							label='비밀번호'
 							setValues={setValues}
+							condition='8자 이상 (대소문자, 특수 문자, 숫자 포함)'
+							error={errors.password}
 						/>
 					</li>
 					<li>
@@ -54,10 +66,16 @@ function UserEdit() {
 							name='passwordConfirm'
 							label='비밀번호 확인'
 							setValues={setValues}
+							error={errors.passwordConfirm}
 						/>
 					</li>
 					<li>
-						<UserButton type='submit' onClick={() => console.log('edit')}>
+						<UserButton
+							type='submit'
+							onClick={e => {
+								e.preventDefault();
+								userValidator(values);
+							}}>
 							수정하기
 						</UserButton>
 					</li>
