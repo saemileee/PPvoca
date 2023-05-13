4e0bc67d4538fb75d394c62a95ac0813fca58609
icallitnewart <icallitnewart@gmail.com>
2023-05-13 10:16:20 +0900

    Feat: Register 페이지 유효성 검사 적용
    Register 페이지 유효성 검사 적용, 추후 api 연결 후 수정 예정

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import useUserValidator from '../hooks/useUserValidator';
 import styles from '../components/User/Register.module.scss';
 import Logo from '../components/common/Logo/Logo';
 import UserInput from '../components/User/UserInput/UserInput';
@@ -16,8 +17,13 @@ function Register() {
 		password: '',
 		passwordConfirm: '',
 	};
-
 	const [values, setValues] = useState(initValues);
+	const { errors, setErrors, userValidator, validationPass } =
+		useUserValidator(initValues);
+
+	useEffect(() => {
+		if (validationPass) alert('유효성 검사 통과!!!');
+	}, [validationPass]);
 
 	return (
 		<main className={styles.container}>
@@ -30,6 +36,7 @@ function Register() {
 							name='email'
 							label='이메일'
 							setValues={setValues}
+							error={errors.email}
 						/>
 					</li>
 					<li>
@@ -38,6 +45,8 @@ function Register() {
 							name='nickname'
 							label='닉네임'
 							setValues={setValues}
+							condition='10자 이하 (특수 문자, 공백 제외)'
+							error={errors.nickname}
 						/>
 					</li>
 					<li>
@@ -45,7 +54,9 @@ function Register() {
 							type='password'
 							name='password'
 							label='비밀번호'
+							condition='8자 이상 (대소문자, 특수 문자, 숫자 포함)'
 							setValues={setValues}
+							error={errors.password}
 						/>
 					</li>
 					<li>
@@ -54,10 +65,16 @@ function Register() {
 							name='passwordConfirm'
 							label='비밀번호 확인'
 							setValues={setValues}
+							error={errors.passwordConfirm}
 						/>
 					</li>
 					<li>
-						<UserButton type='submit' onClick={() => console.log('join')}>
+						<UserButton
+							type='submit'
+							onClick={e => {
+								e.preventDefault();
+								userValidator(values);
+							}}>
 							가입하기
 						</UserButton>
 					</li>
