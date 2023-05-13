a8aee4fb2c90d34c254c452e106707911d5dc84b
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-05-13 16:04:41 +0900

    Refactor: 모달창 모듈화 및 아이콘 변경

--- src/pages/BookList.tsx
+++ src/pages/BookList.tsx
@@ -1,18 +1,16 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../components/BookList/Style.module.scss';
 import AddButton from '../components/common/AddButton/AddButton';
+import Modal from '../components/BookList/BookListModal';
 import { BsFileEarmarkWord } from 'react-icons/bs';
 import { BiMessageSquareCheck, BiMessageSquareError } from 'react-icons/bi';
 import { CiMenuKebab } from 'react-icons/ci';
 import { IoSearchOutline } from 'react-icons/io5';
-import { AiOutlineDelete } from 'react-icons/ai';
-import { HiOutlinePencil } from 'react-icons/hi';
 
 function BookList() {
 	const navigate = useNavigate();
 	const [isModalOpen, setIsModalOpen] = useState(false);
-	const modalRef = useRef<HTMLDivElement>(null);
 
 	const handleMenuClick = (event: React.MouseEvent<HTMLDivElement>) => {
 		event.stopPropagation();
@@ -20,39 +18,33 @@ function BookList() {
 		setIsModalOpen(true);
 	};
 
-	const handleClickOutside = (event: MouseEvent) => {
-		if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-			setIsModalOpen(false);
-		}
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
 	};
-
-	useEffect(() => {
-		document.addEventListener('mousedown', handleClickOutside);
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside);
-		};
-	}, []);
 
 	function handleBoxClick() {
 		navigate('/word/list');
 	}
 
 	function handleEdit() {
-		// 수정 기능 추가
-		setIsModalOpen(false);
+		// 수정 기능 추가 예정
+		handleCloseModal();
 	}
 
 	function handleDelete() {
-		// 삭제 기능 추가
-		setIsModalOpen(false);
+		// 삭제 기능 추가 예정
+		handleCloseModal();
 	}
 
 	return (
 		<main>
-			<div className={styles.inputContainer}>
-				<input className={styles.input} type='text' placeholder='단어 검색' />
-				<div className={styles.searchIcon} onClick={() => alert('검색 버튼')}>
-					<IoSearchOutline size={20} />
+			<div
+				className={styles.inputContainer}
+				onClick={() => navigate('/word/all')}
+			>
+				<div className={styles.input}>단어 검색</div>
+				<div className={styles.searchIcon}>
+					<IoSearchOutline size={24} />
 				</div>
 			</div>
 			<div className={styles.boxContainer}>
@@ -60,30 +52,24 @@ function BookList() {
 					<div className={styles.wordTitle}>영어</div>
 					<div className={styles.subContent}>English / Korean</div>
 					<div onClick={handleMenuClick} className={styles.modalButton}>
-						<CiMenuKebab />
+						<CiMenuKebab size={24} />
 					</div>
-					{isModalOpen && (
-						<div ref={modalRef} className={styles.modal}>
-							<button onClick={handleEdit}>
-								<AiOutlineDelete />
-								수정
-							</button>
-							<button onClick={handleDelete}>
-								<HiOutlinePencil />
-								삭제
-							</button>
-						</div>
-					)}
+					<Modal
+						isOpen={isModalOpen}
+						onClose={handleCloseModal}
+						onEdit={handleEdit}
+						onDelete={handleDelete}
+					/>
 					<div className={styles.wordButton}>
-						<div>
-							<BsFileEarmarkWord />
+						<div className={styles.totalWord}>
+							<BsFileEarmarkWord size={24} />
 							10
 						</div>
 						<div>
-							<BiMessageSquareCheck />0
+							<BiMessageSquareCheck size={24} />0
 						</div>
 						<div>
-							<BiMessageSquareError />0
+							<BiMessageSquareError size={24} />0
 						</div>
 					</div>
 				</div>
