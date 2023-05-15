e241a645d9fb4d5b799770c191a51f06afa404a9
icallitnewart <icallitnewart@gmail.com>
2023-05-15 18:36:25 +0900

    Fix: Login, Register 페이지 토큰이 있는 경우 회원정보 수정 페이지로 리다이렉트
    토큰 여부로 판단하기 보단 토큰 검증 후 사용자 인증 여부로 판단하는 방식으로 수정 예정

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { userTokenState } from '../recoil/userState';
 import styles from '../components/Register/Register.module.scss';
 import Logo from '../components/common/Logo/Logo';
 import RegisterForm from '../components/Register/RegisterForm';
 
 function Register() {
+	const userToken = useRecoilValue(userTokenState);
+	const navigate = useNavigate();
 	const logoStyle = {
 		transform: 'translateX(-10px)',
 		marginBottom: '5vh',
 	};
+
+	useEffect(() => {
+		if (userToken) navigate('/user/info');
+	}, []);
 
 	return (
 		<main className={styles.container}>
