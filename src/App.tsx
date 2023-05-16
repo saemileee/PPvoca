615517efb3e1cc6abc4aac8760b4d118de18b845
icallitnewart <icallitnewart@gmail.com>
2023-05-17 02:57:05 +0900

    Fix: Header 공통 적용되지 않게 App.tsx에서 제거
    Header가 필요한 페이지에서 import 필요

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ function App() {
 	return (
 		<BrowserRouter>
 			<React.Fragment>
-				<Header />
 				<Routes>
 					<Route path='/' element={<Navigate to='/book/list' />} />
 					<Route path='*' element={<Navigate to='/' />} />
