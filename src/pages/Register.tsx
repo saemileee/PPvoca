3faca380e3b4e92bbc403676fb5cb25e4edc5007
icallitnewart <icallitnewart@gmail.com>
2023-05-21 02:23:52 +0900

    Fix: Register 페이지 alert창 모달로 변경

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { userTokenState } from '../recoil/userState';
+import useUserAlert from '../hooks/useUserAlert';
 import styles from '../components/Register/Register.module.scss';
 import Navigation from '../components/common/Navigation/Navigation';
 import Logo from '../components/common/Logo/Logo';
 import RegisterForm from '../components/Register/RegisterForm';
+import AlertModal from '../components/common/AlertModal/AlertModal';
 
 function Register() {
 	const userToken = useRecoilValue(userTokenState);
 	const navigate = useNavigate();
+	const { alertModal, handleCloseAlert, handleOpenAlert } = useUserAlert();
+
 	const logoStyle = {
 		transform: 'translateX(-10px)',
 		marginBottom: '5vh',
@@ -24,7 +28,14 @@ function Register() {
 			<Navigation />
 			<main className={styles.container}>
 				<Logo style={logoStyle} />
-				<RegisterForm />
+				<RegisterForm openAlert={handleOpenAlert} />
+				{alertModal.isOpen && (
+					<AlertModal
+						isOpen={alertModal.isOpen}
+						onClose={alertModal.onClose ? alertModal.onClose : handleCloseAlert}
+						message={alertModal.message}
+					/>
+				)}
 			</main>
 		</>
 	);
