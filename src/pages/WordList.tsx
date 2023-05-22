4ef2e24a5ac63519958bc919a8cec737f2bac781
shinmin <shinmin9812@naver.com>
2023-05-22 14:37:36 +0900

    test

--- src/pages/WordList.tsx
+++ src/pages/WordList.tsx
@@ -34,6 +34,7 @@ import {
 	getBookName,
 	findWordById,
 } from '../apis/word';
+import Header from '../components/common/Header/Header';
 
 //BookList에서 Params로 받아올 bookId
 type RouteParams = {
@@ -235,27 +236,31 @@ function WordList() {
 				<div className={styles.container}>
 					<div className={styles.fixed}>
 						<div className={styles.title}>
-							<div className={styles.back} onClick={handleBack}>
-								<MdArrowBackIosNew />
-							</div>
-							<div className={styles.bookName}>{booktitle}</div>
-							<div className={styles.filter} onClick={handleFilter}>
-								<GiSettingsKnobs />
-							</div>
-							<div className={styles.option} onClick={handleOption}>
-								<CiMenuKebab />
-							</div>
-							{optionModal && (
-								<WordListOptionsModal
-									setModalOpen={setOptionModal}
-									wordList={wordList}
-									setWordList={setWordList}
-									setAlertDeleteOpen={setAlertDeleteModalOpen}
-									setAlertUnmarkOpen={setAlertUnmarkModalOpen}
-									setAlertCheckOpen={setAlertCheckModalOpen}
-									setAlertUnknownOpen={setAlertUnknownModalOpen}
-								/>
-							)}
+							<Header
+								title={booktitle}
+								addGoBackButton={true}
+								rightComponent={
+									<div>
+										<div className={styles.filter} onClick={handleFilter}>
+											<GiSettingsKnobs />
+										</div>
+										<div className={styles.option} onClick={handleOption}>
+											<CiMenuKebab />
+										</div>
+										{optionModal && (
+											<WordListOptionsModal
+												setModalOpen={setOptionModal}
+												wordList={wordList}
+												setWordList={setWordList}
+												setAlertDeleteOpen={setAlertDeleteModalOpen}
+												setAlertUnmarkOpen={setAlertUnmarkModalOpen}
+												setAlertCheckOpen={setAlertCheckModalOpen}
+												setAlertUnknownOpen={setAlertUnknownModalOpen}
+											/>
+										)}
+									</div>
+								}
+							/>
 						</div>
 						<div className={styles.search}>
 							<input
@@ -289,8 +294,8 @@ function WordList() {
 										checkedList.length === 0
 											? false
 											: checkedList.length === wordList.length
-											? true
-											: false
+												? true
+												: false
 									}
 								/>
 							</div>
